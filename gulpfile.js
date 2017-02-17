c98e17a7d23b4c98502a45c2b25b6442fe4828c5
Hiroki Kumamoto <kumabook@live.jp>
2017-02-17 13:45:21 +0900

    Fix eslint warnings in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,8 @@ const eslint   = require('gulp-eslint');
 
 gulp.task('lint', () =>
           gulp.src(['lib/**/*.{js,jsx}', 'test/**/*.{js,jsx}'])
-              .pipe(eslint({ fix: true}))
+              .pipe(eslint({ fix: true }))
               .pipe(eslint.format())
-              .pipe(eslint.failAfterError())
-         );
+              .pipe(eslint.failAfterError()));
 
-gulp.task('default', ['lint'], function () {});
+gulp.task('default', ['lint'], () => {});
