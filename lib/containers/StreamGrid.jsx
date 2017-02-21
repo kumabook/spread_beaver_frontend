0ab5c9fa6fdab60e43af2d7bed476cd9712f728d
Hiroki Kumamoto <kumabook@live.jp>
2017-02-21 10:23:22 +0900

    StreamGrid shows an entry grid view

--- lib/containers/StreamGrid.jsx
+++ lib/containers/StreamGrid.jsx
@@ -1,22 +1,42 @@
-import React          from 'react';
-import { connect }    from 'react-redux';
+import React                  from 'react';
+import { connect }            from 'react-redux';
+import { GridList, GridTile } from 'material-ui/GridList';
+import IconButton             from 'material-ui/IconButton';
+import StarBorder             from 'material-ui/svg-icons/toggle/star-border';
 
 class StreamGrid extends React.Component {
   static get propTypes() {
     return {
-      streamId: React.PropTypes.string.isRequired,
+      entries: React.PropTypes.array.isRequired,
     };
   }
   componentDidMount() {
   }
   render() {
-    return <div>{this.props.streamId}</div>;
+    return (
+      <GridList
+        cellHeight={180}
+        style={{
+        }}
+      >
+        {this.props.entries.map(entry => (
+          <GridTile
+            key={entry.id}
+            title={entry.title}
+            subtitle={<span>by <b>{entry.author}</b></span>}
+            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+          >
+            <img src={entry.visual.url} alt={entry.title} />
+          </GridTile>
+         ))}
+      </GridList>
+    );
   }
 }
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
   return {
-    streamId: ownProps.params.splat,
+    entries: state.entries.items,
   };
 }
 
